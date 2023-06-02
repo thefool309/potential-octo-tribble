f5490c7ee9c12e75e908b60bde636b6e54bb4259
thefool309 <104532635+thefool309@users.noreply.github.com>
2023-06-02 16:49:00 -0500

    Update defining_and_calling_functions.cpp

--- cppfiles/defining_and_calling_functions.cpp
+++ cppfiles/defining_and_calling_functions.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-// run this program using the console pauser or add your own getch, system("pause") or input loop 
 
 int compare(int firstnum, int secondnum)
 {
